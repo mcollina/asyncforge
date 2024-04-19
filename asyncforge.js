ff6b1fa339928f6164176d8e24ed7207f488bbc1
Matteo Collina <hello@matteocollina.com>
2024-04-19 15:47:07 +0200

    added memo
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- asyncforge.js
+++ asyncforge.js
@@ -5,14 +5,14 @@ const { AsyncLocalStorage } = require('node:async_hooks')
 const asyncLocalStorage = new AsyncLocalStorage();
 
 function start (config) {
-  const store = {
-    config
-  }
+  const store = Object.create(null)
+  store.config = config
   asyncLocalStorage.enterWith(store)
 }
 
+let forgeCounter = 0
 function forge (fn) {
-  const sym = Symbol('forge.' + (fn.name || 'anonymous'))
+  const sym = Symbol('forge.' + (fn.name || forgeCounter++))
   return function () {
     const store = asyncLocalStorage.getStore()
     if (store[sym]) {
@@ -24,5 +24,29 @@ function forge (fn) {
   }
 }
 
+let memoCounter = 0
+function memo (name) {
+  name = name || 'memo' + memoCounter++
+
+  const sym = Symbol('memo.' + name)
+
+  function get () {
+    const store = asyncLocalStorage.getStore()
+    return store[sym]
+  }
+
+  function set (value) {
+    let store = asyncLocalStorage.getStore()
+    store = Object.create(store)
+    store[sym] = value
+    asyncLocalStorage.enterWith(store)
+  }
+
+  get.set = set
+
+  return get
+}
+
 module.exports.start = start
 module.exports.forge = forge
+module.exports.memo = memo
